0d13b3d1b11786a4b69629124c950da4ab884b62
David Leon <davidleon.it@gmail.com>
2019-12-06 00:37:24 -0600

    Models, Controllers and DB

---
+++ app/TypePerson.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypePerson extends Model
+{
+    //
+}
---
+++ database/migrations/2019_12_06_015838_create_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date_request');
+            $table->unsignedInteger('type_request_id');
+            $table->foreign('type_request_id')->references('id')->on('type_requests')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
